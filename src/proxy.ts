0cec180b41391a46f60665e8ba14b0c45e3f9fa9
Luiz Lessa <lc.lessa@gmail.com>
2026-05-01 20:48:25 -0300

    use proxy for basic auth

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,34 @@
 import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
+function checkBasicAuth(request: NextRequest) {
+  const basicAuth = request.headers.get("authorization");
+
+  const username = process.env.BASIC_AUTH_USER;
+  const password = process.env.BASIC_AUTH_PASSWORD;
+
+  if (!username || !password) return false;
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(" ")[1];
+    const [user, pwd] = atob(authValue).split(":");
+
+    return user === username && pwd === password;
+  }
+
+  return false;
+}
+
 export async function proxy(request: NextRequest) {
+  if (!checkBasicAuth(request)) {
+    return new NextResponse("Autenticação necessária", {
+      status: 401,
+      headers: {
+        "WWW-Authenticate": 'Basic realm="Área protegida"',
+      },
+    });
+  }
+
   let supabaseResponse = NextResponse.next({ request });
 
   const supabase = createServerClient(
@@ -25,7 +52,6 @@ export async function proxy(request: NextRequest) {
     }
   );
 
-  // Refresh session
   await supabase.auth.getUser();
 
   return supabaseResponse;
@@ -33,6 +59,6 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
